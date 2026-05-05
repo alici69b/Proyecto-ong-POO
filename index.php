650dcd5d860b2ef05a2f74bf3eef3f117db3d355
Antonio Jesús Carrasco <antjescar1995@gmail.com>
2026-05-05 12:23:25 +0200

    Resuelvo conflictos de merge

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//Cargamos la clase de la base de datos
+require_once "app/config/db.php";
+
+$db = new Database();
+$connection = $db->getConnection();
+
+
+
+
+//Cargamos la vista principal
+echo "biennnnnnnnnnnnn";
+?>

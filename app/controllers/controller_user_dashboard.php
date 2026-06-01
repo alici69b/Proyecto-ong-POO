b353eba09cfaa897620338b4ccfcc0a519ab6c1f
alicia alonso <alicialonsoaguaded@gmail.com>
2026-06-01 12:53:36 +0200

    fix: simulados

--- app/controllers/controller_user_dashboard.php
+++ app/controllers/controller_user_dashboard.php
@@ -3,4 +3,10 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 
 $modo_simulado = isset($_SESSION['modo_simulado']) && $_SESSION['modo_simulado'];
 
+$nombreCompleto = htmlspecialchars($_SESSION['user_nombre'] ?? 'Usuario');
+$totalResets = 0;
+$enCurso = 0;
+$logrados = 0;
+$resets = [];
+
 require_once __DIR__ . "/../views/user/dashboard.php";
